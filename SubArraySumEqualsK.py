9932f6bf351ed7fb5948706368ccf96ecc13383b
Deep Contractor <31130563+chesterking123@users.noreply.github.com>
2020-10-20 13:18:10 +0530

    Update SubArraySumEqualsK.py

--- SubArraySumEqualsK.py
+++ SubArraySumEqualsK.py
@@ -13,4 +13,34 @@ class Solution(object):
             print(sumdict)
         return count
         
-        
+
+#Print Subarray
+def subArraySum(arr, n, sum): 
+ 
+    curr_sum = arr[0] 
+    start = 0
+    i = 1
+    while i <= n: 
+        while curr_sum > sum and start < i-1: 
+            print(curr_sum)
+            curr_sum = curr_sum - arr[start] 
+            start += 1
+              
+        if curr_sum == sum: 
+            print ("Sum found between indexes") 
+            print (arr[start:i]) 
+            return 
+
+        if i < n: 
+            curr_sum = curr_sum + arr[i] 
+        i += 1
+  
+    print ("No subarray found") 
+    return 0
+  
+# Driver program 
+arr = [15, 2, 4, 8, 9, 5, 10, 23] 
+n = len(arr) 
+sum = 23
+  
+subArraySum(arr, n, sum)
