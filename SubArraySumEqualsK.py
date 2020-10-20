151cb442513c669c87b35289535b1e741ece3125
Deep Contractor <31130563+chesterking123@users.noreply.github.com>
2020-10-20 13:23:02 +0530

    Update SubArraySumEqualsK.py

--- SubArraySumEqualsK.py
+++ SubArraySumEqualsK.py
@@ -38,9 +38,31 @@ def subArraySum(arr, n, sum):
     print ("No subarray found") 
     return 0
   
-# Driver program 
-arr = [15, 2, 4, 8, 9, 5, 10, 23] 
-n = len(arr) 
-sum = 23
-  
-subArraySum(arr, n, sum)
+#
+#
+#
+#
+# This program can handel neg values
+def subArraySum(arr, n, Sum):  
+
+    Map = {}  
+
+    curr_sum = 0 
+    
+    for i in range(0,n):  
+
+        curr_sum = curr_sum + arr[i]  
+
+        if curr_sum == Sum:  
+           
+            print("Sum found between indexes 0 to", i) 
+            return  
+        if (curr_sum - Sum) in Map:  
+           
+            print("Sum found between indexes", \ 
+                   Map[curr_sum - Sum] + 1, "to", i)  
+              
+            return 
+    
+        Map[curr_sum] = i   
+    print("No subarray with given sum exists")
